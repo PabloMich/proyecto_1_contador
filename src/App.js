ca51deea9bb196168e0f03669765612324ee349d
Pablo Mich <pabloemich@gmail.com>
2022-04-15 17:29:43 -0600

    subiendo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      contador : 0
+    }; 
+  }
+  sumar(){
+    this.setState ({
+       contador : this.state.contador+1,
+       
+    });
+  }
+  restar(){
+    this.setState({
+       contador : this.state.contador-1
+    });
+  }
+  reset(){
+    this.setState({
+       contador : 0,
+    });
+  }
+  render(){
+    return (
+      <body>
+      <div class="contenedor">
+        <p class="titulo">Contador</p>
+        <p class="contar" id="contar"><Contador valor={this.state.contador}/></p>
+    
+        <div class="botones">
+          <button class="incr" id="incr" onClick={()=>this.sumar()}>
+            <span class="shadow"></span>
+            <span class="edge"></span>
+            <span class="front text"> INCREASE 
+            </span>
+          </button>
+          <button class="decr" id="decr" onClick={()=>this.restar()}> 
+            <span class="shadow"></span>
+            <span class="edge"></span>
+            <span class="front text"> DECREASE
+            </span>
+          </button>
+          <button class="reset" id="reset" onClick={()=>this.reset()}>
+            <span class="shadow"></span>
+            <span class="edge"></span>
+            <span class="front text"> RESET
+            </span>
+          </button>
+        </div>
+      </div>
+    </body>
+    )
+  }
 }
+//Functional Component
+const Contador = props => {
+  if (props.valor > 0) 
+  return(
+    <div class={props.valor > 0 ? "positivo" : "negativo"}>{props.valor}</div>
+  );
+  else if (props.valor < 0)
+  return(
+    <div class={props.valor > 0 ? "positivo" : "negativo"}>{props.valor}</div>
+  );
+  else if (props.valor === 0)
+  return(
+    <div class={props.valor === 0 ? "reset" : "reset"}>{props.valor}</div>
+  );
+};
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
 
 export default App;
